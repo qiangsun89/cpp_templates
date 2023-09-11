332bac10a0a7c86ef0eff8d6252102b935580f51
Qiang Sun <sunqiang891103@gmail.com>
2023-09-11 11:03:06 +0800

    add chapter09

---
+++ chapter09/myfirst2/myfirstmain.cpp
@@ -0,0 +1,10 @@
+#include "myfirst2.hpp"
+
+// use of the template
+int main()
+{
+    // double ice = 3.0;
+    // int  ice = 2;
+    float  ice = 2;
+    printTypeof(ice);   // call function template for type double
+}
